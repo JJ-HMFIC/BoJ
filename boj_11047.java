56a67aa5bca42d5b5b0bf7fcfeb9a2abbfd92fbd
Kyunghwan Jeon <crasheso@gmail.com>
2024-12-13 18:27:07 +0900

    greedy basic

---
+++ boj_11047.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class boj_11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] A = new int[N];
+        int cnt = 0;
+        for (int i = 0; i < N; i++) A[i] = sc.nextInt();
+        for (int i = N-1; i >=0 ; i--) {
+            if (A[i] <= K) {
+                cnt += (K / A[i]);
+                K = K % A[i];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
