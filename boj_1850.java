0e5ba71d37e8fce3d9dced4d9acd0e01900d53d0
Kyunghwan Jeon <crasheso@gmail.com>
2024-12-30 22:52:46 +0900

    gcd

---
+++ boj_1850.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class boj_1850 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long result = gcd(a, b);
+        while (result > 0) {
+            bw.write("1");
+            result--;
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) return a;
+        else return gcd(b, a % b);
+    }
+}
