e07907c376bd94659edf605768bbf400723241d8
Kyunghwan Jeon <crasheso@gmail.com>
2025-04-19 15:08:22 +0900

    fix: early return reflected

--- kb_algorithm/week1/pro_maraton.java
+++ kb_algorithm/week1/pro_maraton.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class pro_maraton {
     public String solution(String[] participant, String[] completion) {
+        /*
         HashMap<String, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < participant.length; i++) {
             if (!hashMap.containsKey(participant[i])) {
@@ -22,5 +23,23 @@ public class pro_maraton {
         }
 
         return "";
+
+         */
+        // 얼리 리턴 방식
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (String tmp : completion) {
+            hashMap.compute(tmp, (key, value) -> (value == null) ? 1 : value + 1);
+        } // completion의 요소를 순회하여 map 에 key,value로 저장
+
+        for (String part : participant){
+            // 참가자가 완주자 명단에 없다 or value 값이 0이다 => 정답
+            if(!hashMap.containsKey(part) || hashMap.get(part)==0) return part;
+
+            hashMap.compute(part, (key, value) -> value - 1);
+            // 명단의 수 -1 계산
+        }
+
+        return null;
     }
 }
