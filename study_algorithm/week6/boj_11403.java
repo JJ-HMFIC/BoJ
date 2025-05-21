107e2fae97133bf12b37cd7890803deb4b5e7ff3
Kyunghwan Jeon <crasheso@gmail.com>
2025-05-22 01:16:59 +0900

    add) #11403 solved

--- study_algorithm/week6/boj_11403.java
+++ study_algorithm/week6/boj_11403.java
@@ -1,7 +1,38 @@
 package study_algorithm.week6;
 
-public class boj_11403 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class boj_11403 {
+    static int N;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        int[][] distance = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                distance[i][j] = v;
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(distance[i][k]==1 && distance[k][j]==1) distance[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(distance[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
