966afb2740780ad2c60691f78a05ca398ba97850
Kyunghwan Jeon <crasheso@gmail.com>
2025-06-19 01:49:47 +0900

    per, com comment added

--- kb_algorithm/week6/leet_combiantions.java
+++ kb_algorithm/week6/leet_combiantions.java
@@ -16,13 +16,17 @@ class Solution {
         if (integers.size() == k) {
             answer.add(new ArrayList<>(integers));
             return;
+            //사이즈가 k라면 -> 순열과 다른 점
         }
 
         for (int i = start; i <= n; i++) {
 //            if (!integers.isEmpty() && i <= integers.get(0)) continue;
             integers.add(i);
+            // 요소를 담고
             Com(n, k, i+1,integers);
+            // 현재 요소보다 큰 요소부터 조합 돌리기 -> 중복 방지
             integers.remove(integers.size() - 1);
+            // 백트래킹 처리
         }
     }
 }
--- kb_algorithm/week6/leet_permuatations.java
+++ kb_algorithm/week6/leet_permuatations.java
@@ -13,18 +13,19 @@ class Solution {
     }
 
     private void Per(List<List<Integer>> answers, int[] nums, ArrayList<Integer> integers) {
-        if (integers.size() == nums.length) {
+        if (integers.size() == nums.length) { // 순서를 다 정했으면
             answers.add(new ArrayList<>(integers));
-            return;
+            return; // 순열의 경우의 수 담기
         }
 
         for (int i = 0; i < nums.length; i++) {
             if (!visited[i]) {
-                visited[i] = true;
-                integers.add(nums[i]);
-                Per(answers,nums,integers);
+                visited[i] = true; // 방문하고
+                integers.add(nums[i]); //담고
+                Per(answers,nums,integers); // dfs돌리고
                 integers.remove(integers.size() - 1);
                 visited[i] = false;
+                // 백트래킹 처리
             }
         }
 
