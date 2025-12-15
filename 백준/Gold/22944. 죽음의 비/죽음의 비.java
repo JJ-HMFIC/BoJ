41a7042a7f311187464049e38184f399c8db6fda
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2025-12-16 00:43:11 +0900

    [Gold III] Title: 죽음의 비, Time: 396 ms, Memory: 41868 KB -BaekjoonHub

--- 백준/Gold/22944. 죽음의 비/죽음의 비.java
+++ 백준/Gold/22944. 죽음의 비/죽음의 비.java
@@ -1,97 +1,87 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n,h,d;
-	static char[][] map;
-	static int[][] check;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	static class Node{
-		int x, y, h, cost, cnt;
-		public Node(int x, int y, int h, int cost, int cnt) {
-			this.x = x;
-			this.y = y;
-			this.h = h;
-			this.cost = cost;
-			this.cnt = cnt;
-		}
-	}
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		h = Integer.parseInt(st.nextToken());
-		d = Integer.parseInt(st.nextToken());
-		
-		map = new char[n][n];
-		check = new int[n][n];
-		
-		int startX=0, startY=0;
-		for(int i=0; i<n; i++) {
-			String str = br.readLine();
-			for(int j=0; j<str.length(); j++) {
-				map[i][j] = str.charAt(j);
-				if(map[i][j]=='S') {
-					startX = i;
-					startY = j;
-				}
-			}
-		}
-		
-		int ans = BFS(startX, startY);
-		System.out.println(ans);
-	}
-	
-	//sx,sy : 시작위치
-	public static int BFS(int sx, int sy) {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(sx,sy,h,0,0));//시작점 추가
-		check[sx][sy] = h;//시작점 방문처리
-		
-		int min = Integer.MAX_VALUE;//최소이동 거리
-		while(!q.isEmpty()) {
-			Node cur = q.poll();
-			
-			//상하좌우 탐색
-			for(int i=0; i<4; i++) {
-				int hp=cur.h, cost=cur.cost, cnt=cur.cnt;
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				//경계 바깥
-				if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
-				
-				//E에 도달했으면 min갱신
-				if(map[nx][ny]=='E') {
-					min = Math.min(min, cnt+1);
-					continue;
-				}
-				
-				//우산이면 갈아치우기
-				if(map[nx][ny]=='U') cost = d;
-				
-				//E,U는 위에서 처리했기 때문에 남은건 . 밖에 없음.
-				if(cost!=0) cost--; //우산 내구도 있을떄
-				else hp--; //없을떄
-				
-				//죽는경우는 다시 탐색
-				if(hp==0) continue;
-				
-				//현재 요소가 전에 방문했던 요소보다 진행을 더 할수 있다 = 현재요소로 방문처리하기
-				if(check[nx][ny]<hp+cost) {
-					check[nx][ny] = hp+cost;
-					q.add(new Node(nx, ny, hp, cost, cnt+1));
-				}
-			}
-		}//while
-		
-		if(min==Integer.MAX_VALUE) return -1;//E에 도달 못함
-		return min;//E에 도달함.
-	}
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static char[][] arr;
+    static int[][] visited;
+    static int x, y, N, H, D;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        arr = new char[N][N];
+        visited = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            arr[i] = line.toCharArray();
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 'S') {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        System.out.println(findE(x, y));
+    }
+
+    private static int findE(int x, int y) {
+        Queue<Info> queue = new ArrayDeque<>();
+        visited[x][y] = H;
+        queue.add(new Info(x, y, H, 0, 0));
+
+        int min = Integer.MAX_VALUE;
+        while (!queue.isEmpty()) {
+            Info top = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = top.x + dx[i];
+                int ny = top.y + dy[i];
+                if (nx >= N || ny >= N || nx < 0 || ny < 0) continue;
+
+                int nextUm = top.um;
+                int nextHp = top.hp;
+                if (arr[nx][ny] == 'E') {
+                    min = Math.min(min, top.dist + 1);
+                }
+                if (arr[nx][ny] == 'U') nextUm = D;
+
+                if (nextUm > 0) nextUm--; // 다음 행선지에 대한 정보이므로
+                else nextHp--;
+                
+
+                if (nextHp == 0) continue;
+                if (visited[nx][ny] < nextHp + nextUm) { // 현재 경우의 수가 더 체력이 많으면(최적해)
+                    visited[nx][ny] = nextHp + nextUm; // 업데이트
+                    queue.add(new Info(nx, ny, nextHp, nextUm, top.dist + 1));
+                }
+            }
+        }
+        if (min == Integer.MAX_VALUE) min = -1;
+        return min;
+    }
+
+    static class Info {
+        int x;
+        int y;
+        int hp;
+        int um;
+        int dist;
+
+        public Info(int x, int y, int hp, int um, int dist) {
+            this.x = x;
+            this.y = y;
+            this.hp = hp;
+            this.um = um;
+            this.dist = dist;
+        }
+    }
 }
+
+
