54367f2c576b851780063454fac681be2a5f79c4
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2026-02-05 19:47:47 +0900

    [Gold II] Title: 반도체 설계, Time: 224 ms, Memory: 21500 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : arr) {
+            if (list.isEmpty() || list.get(list.size() - 1) < num) {
+                list.add(num);
+            } else {
+                int pos = Collections.binarySearch(list, num);
+                if (pos < 0) {
+                    pos = -(pos + 1);
+                }
+                list.set(pos, num);
+            }
+        }
+        System.out.println(list.size());
+    }
+}
