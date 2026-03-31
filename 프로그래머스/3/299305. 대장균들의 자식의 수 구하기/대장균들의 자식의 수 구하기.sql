9dffdf33aaf9d6253c93f7510ab0d131eac594b7
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2026-03-31 14:03:59 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -3,3 +3,12 @@ select a.id, ifnull(count(b.id),0) as 'child_count'
 from ECOLI_DATA a left join ECOLI_DATA b
 on a.id = b.parent_id
 group by a.id
+
+# SELECT 
+#     A.ID, 
+#     COUNT(B.ID) AS CHILD_COUNT  -- COUNT(컬럼명)은 NULL을 0으로 세주기 때문에 IFNULL이 필수는 아니에요!
+# FROM ECOLI_DATA A 
+# LEFT JOIN ECOLI_DATA B          -- 모든 A(부모 후보)를 다 살리기 위해 LEFT JOIN 사용
+#     ON A.ID = B.PARENT_ID
+# GROUP BY A.ID
+# ORDER BY A.ID;                  -- 마지막에 ID 기준 정렬도 잊지 마세요!
