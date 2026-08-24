a2b869c98372fb8dfd9d1324d52ba865984df643
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2026-08-24 16:48:49 +0900

    [level 2] Title: 배달, Time: 4.70 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,67 +1,30 @@
 import java.util.*;
 class Solution {
-    static ArrayList<Edge>[] list;
-    static int[] distance;
-    static boolean[] visited;
     public int solution(int N, int[][] road, int K) {
-        list = new ArrayList[N+1];
-        for(int i = 1 ; i<=N ;i++){
-            list[i] = new ArrayList<>();
+        int answer = 0;
+        int[][] map = new int[N+1][N+1];
+        for(int i =1 ; i<=N;i++){
+            Arrays.fill(map[i],10000000);
+            map[i][i] = 0;
         }
         for(int[] info : road){
             int a = info[0];
-            int b= info[1];
+            int b = info[1];
             int w = info[2];
-            
-            list[a].add(new Edge(b,w));
-            list[b].add(new Edge(a,w));
+            map[a][b] = Math.min(map[a][b],w);
+            map[b][a] = Math.min(map[b][a],w);
         }
-        distance = new int[N+1];
-        Arrays.fill(distance,Integer.MAX_VALUE);
-        
-        
-        visited= new boolean[N+1];
-        dijkstra(1);
-        int answer = 0;
-        for(int w : distance){
-            if(w<=K) answer++;
-        }
-
-
-        return answer;
-    }
-    public void dijkstra(int start){
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        distance[1] = 0;
-        pq.add(new Edge(start,0));
-        
-        while(!pq.isEmpty()){
-            Edge top = pq.poll();
-            int v = top.v;
-            int w = top.w;
-            if(visited[v]) continue;
-            visited[v] = true;
-            for(Edge next : list[v]){
-                int nv = next.v;
-                int nw = next.w;
-                
-                if(distance[nv]>distance[v]+nw){
-                    distance[nv] = distance[v]+nw;
-                    pq.add(new Edge(nv,distance[v]+nw));
+        for(int k = 1; k<=N;k++){
+            for(int i = 1; i<=N; i++){
+                for(int j = 1; j<=N;j++){
+                    map[i][j] = Math.min(map[i][j], map[i][k]+map[k][j]);
                 }
             }
         }
-    }
-    public static class Edge implements Comparable <Edge>{
-        int v;
-        int w;
-        public Edge(int v, int w){
-            this.v = v;
-            this.w = w;
+        for(int i = 1; i<=N;i++){
+            if(map[1][i]<=K) answer++;
         }
-        public int compareTo(Edge e){
-            return Integer.compare(this.w, e.w);
-        }
+        return answer;
+        
     }
-
 }
