f051cc42f30f62bb765194c3b524b0376cbafa4d
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2026-08-26 23:38:01 +0900

    [level 3] Title: 합승 택시 요금, Time: 16.49 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,37 +1,71 @@
 import java.util.*;
-
 class Solution {
+    static ArrayList<Grid>[] list ;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int[][] map = new int[n+1][n+1];
-        int INF = 100000*200;
-        for(int i = 1 ; i<=n;i++){
-            Arrays.fill(map[i], INF);
-            map[i][i] = 0;
+        
+        list = new ArrayList[n+1];
+        
+        
+        for(int i = 1; i<=n ;i++){
+            list[i] = new ArrayList<>();
+        }
+        for(int [] fare : fares){
+            int x = fare[0];
+            int y = fare[1];
+            int cost = fare[2];
+            list[x].add(new Grid(y,cost));
+            list[y].add(new Grid(x,cost));
         }
         
-        for(int i = 0 ; i<fares.length;i++){
-            int start = fares[i][0];
-            int end = fares[i][1];
-            int cost = fares[i][2];
-            
-            map[start][end] = cost;
-            map[end][start] = cost;
+        int[] dists = dijkstra(s, n);
+        int[] dista = dijkstra(a, n);
+        int[] distb = dijkstra(b, n);
+        
+        int answer = Integer.MAX_VALUE;
+        for(int i = 1 ; i<=n ;i++){
+            int cost = dists[i] + dista[i] + distb[i];
+            answer = Math.min(cost,answer);
         }
-        for(int k = 1; k<=n;k++){
-            for(int i = 1 ; i<=n;i++){
-                for(int j = 1 ; j<=n;j++){
-                    if(map[i][k]!=INF && map[k][j]!=INF){
-                        map[i][j] = Math.min(map[i][j], map[i][k]+map[k][j]);
-                    }
+        return answer;
+      
+    }
+    public int[] dijkstra(int start, int n){
+        PriorityQueue<Grid> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[n+1];
+        int[] distance = new int[n+1];
+        
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start] = 0;
+        pq.add(new Grid(start, 0));
+        
+        while(!pq.isEmpty()){
+            Grid top = pq.poll();
+            int v = top.v;
+            int w = top.w;
+            if(visited[v]) continue;
+            visited[v] = true;
+            
+            for(Grid next : list[v]){
+                int nv = next.v;
+                int nw = next.w;
+                if(distance[nv]>distance[v]+nw){
+                    distance[nv] = distance[v]+nw;
+                    pq.add(new Grid(nv, distance[nv]));
                 }
             }
+            
         }
-        int answer = INF;
-        for(int i = 1 ; i<=n ;i++){
-            int cost = map[s][i] + map[i][a] + map[i][b];
-            answer = Math.min(answer, cost);
-        }
-        
-        return answer;
+        return distance;
     }
+    public class Grid implements Comparable<Grid>{
+        int v;
+        int w;
+        public Grid(int v,int w){
+            this.v = v;
+            this.w = w;
+        }
+        public int compareTo(Grid o){
+            return Integer.compare(this.w, o.w);
+        }
+    } 
 }
