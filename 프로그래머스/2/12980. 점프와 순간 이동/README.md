6eef1a1b2123bfddb7ff418b83d35c81f969c222
Jeon, Kyunghwan <114416829+JJ-HMFIC@users.noreply.github.com>
2026-09-18 00:12:03 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/README.md
+++ 프로그래머스/2/12980. 점프와 순간 이동/README.md
@@ -1,10 +1,10 @@
 # [level 2] 점프와 순간 이동 - 12980 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980) 
 
 ### 성능 요약
 
-메모리: 57.4 MB, 시간: 0.02 ms
+메모리: 56.9 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 25일 18:46:32
+2026년 09월 18일 00:12:01
 
 ### 문제 설명
 
--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        while(n!=0) {
-            if(n%2 ==0){
-                n = n/2;
-            }else{
-                n = n-1;
-                answer++;
+        int ans = 0;
+
+        while(n!=0){
+            if(n%2==0) n /=2;
+            else{
+                n = (n-1)/2;
+                ans++;
             }
         }
-        return answer;
+
+        return ans;
     }
 }
